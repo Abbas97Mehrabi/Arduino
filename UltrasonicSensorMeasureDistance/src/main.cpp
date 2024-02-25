9e8aff312ecff754d6ee099d9a96ba0ffe1b1253
AbbasMehrabi97 <abbasmehrabi95@gmail.com>
2024-02-25 09:58:50 +0300

    Ultrasonic Led

--- UltrasonicSensorMeasureDistance/src/main.cpp
+++ UltrasonicSensorMeasureDistance/src/main.cpp
@@ -3,12 +3,16 @@
 int trigger_pin = 8;
 int echo_pin = 9;
 
+int green_led = 13;
+int red_led = 12;
 int distance;
 long duration;
 void setup() {
   Serial.begin(9600);
   pinMode(trigger_pin, OUTPUT);
   pinMode(echo_pin, INPUT);
+  pinMode(green_led, OUTPUT);
+  pinMode(red_led, OUTPUT);
 }
 
 void loop() {
@@ -24,5 +28,17 @@ void loop() {
   Serial.print("Distance : ");
   Serial.print(distance);
   Serial.print(" CM \n");
+
+  if (distance < 15)
+  {
+    digitalWrite(red_led, HIGH);
+    digitalWrite(green_led, LOW);
+    Serial.println("Danger");
+  }else{
+    digitalWrite(green_led, HIGH);
+    digitalWrite(red_led, LOW);
+    Serial.println("Safe");
+  }
+  
 }
 
